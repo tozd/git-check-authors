e5dd0d3f17db6afa06da51db6147ad6f087d2855
Mitar <mitar.git@tnode.com>
2016-08-07 12:24:24 -0700

    Initial version.

---
+++ check-authors.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -e
+
+ROOT_COMMIT=$(git rev-list --max-parents=0 HEAD)
+AUTHORS=.gitauthors
+
+# Load known authors.
+declare -A KNOWN_AUTHORS
+for author in $(cat ${AUTHORS}); do
+  KNOWN_AUTHORS[${author}]="ok"
+done
+
+# Check that all authors are present in the authors file.
+echo "Checking whether all commit authors are known..."
+
+failures=0
+for entry in $(git log ${ROOT_COMMIT}.. --format="%H:%aE" --reverse); do
+  IFS=':' read -ra atoms <<< "${entry}"
+  commit=${atoms[0]}
+  author=${atoms[1]}
+  if [ -z "${KNOWN_AUTHORS[${author}]}" ]; then
+    echo "Commit ${commit} has an unknown author '${author}'."
+    failures=1
+  fi
+done
+
+if [ "${failures}" == "0" ]; then
+  echo "All commit authors verified."
+else
+  echo "Unknown authors found."
+  exit 1
+fi
+
